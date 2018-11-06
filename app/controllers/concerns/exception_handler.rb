33f6e808d9814d24e308c002350e9cbd67757700
TomSpencerLondon <tom@homeflow.co.uk>
2018-11-06 07:22:09 +0000

    Add passing AuthenticateUser service tests and exception handlers

--- app/controllers/concerns/exception_handler.rb
+++ app/controllers/concerns/exception_handler.rb
@@ -9,12 +9,26 @@ module ExceptionHandler
   class InvalidToken < StandardError ; end
 
   included do
+    # Define custom handlers
+    rescue_from ActiveRecord::RecordInvalid, with: :four_twenty_two
+    rescue_from ExceptionHandler::AuthenticationError, with: :unauthorized_request
+    rescue_from ExceptionHandler::MissingToken, with: :four_twenty_two
+    rescue_from ExceptionHandler::InvalidToken, with: :four_twenty_two
+
+
     rescue_from ActiveRecord::RecordNotFound do |e|
       json_response({ message: e.message }, :not_found)
     end
+
+    private
+
+    def four_twenty_two(e)
+      json_response({ message: e.message }, unprocessable_entity)
+    end
     
-    rescue_from ActiveRecord::RecordInvalid do |e|
-      json_response({ message: e.message }, :unprocessable_entity)
+    # JSON response with message; Status code 401 - Unauthorized
+    def unauthorized_request(e)
+      json_response({ message: e.message }, :unauthorized)
     end
   end
 end
